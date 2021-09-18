c4f1b92cc4c5f9958b1614c125129795d59ab7b4
ahrimy <dlsdjdkfla@gmail.com>
2021-09-19 01:05:47 +0900

    스프링 적용
    - AppConfig 를 Spring Container 에 등록

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -9,25 +9,33 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // Application 에 대한 환경 구성
 // 생성자 주입
 // DI(Dependency Injection) : 의존관계 주입, 의존성 주입
+
+@Configuration
 public class AppConfig {
 
     // 각각의 역할과 구현 클래스가 한눈에 보임
+    @Bean // Bean : Spring Container 에 등록이 됨, method 이름으로 등록됨
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         // 할인 정책을 변경할때 이곳의 코드만 변경하면 된다.
 //        return new FixDiscountPolicy();
--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -4,14 +4,22 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     // main method 로 테스트하는건 한계가 있다.
     // test code 작성
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        // AppConfig 생성 전
 //        MemberService memberService = new MemberServiceImpl();
+        // AppConfig 생성 후
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+        // Spring 적용
+        // AppConfig 에 Bean 으로 등록된 것들을 관리함
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
