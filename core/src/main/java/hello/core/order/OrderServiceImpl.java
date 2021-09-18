e07bb2c9fb7d19ebbf17d294f475ad18c8d52275
ahrimy <dlsdjdkfla@gmail.com>
2021-09-18 15:20:51 +0900

    주문/할인 도메인 개발 (순수 자바 코드)
    - DefaultPolicy
    - OrderService
    - Test

---
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,23 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+// DiscountPolicy 가 있기 때문에 OrderService 에서는 할인에 대한 구현을 신경쓸 필요 없다.
+// 단일 책임의 원칙이 잘 지켜짐
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
