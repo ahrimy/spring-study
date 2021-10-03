75feef2d7409a9fa3f125502d9c35a77f4ee3684
ahrimy <dlsdjdkfla@gmail.com>
2021-10-03 21:23:11 +0900

    ObjectProvider, ObjectFactory, JSR-330 Provider

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,12 +1,14 @@
 package hello.core.scope;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -34,22 +36,29 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
+        // 컨테이너에서 빈을 찾아주는 역할 (DL: Dependency Lookup)
+        // ObjectFactory -> getObject()
+        // ObjectProvider -> ObjectFactory 상속 + 추가 기능
+        // 둘다 별도의 라이브러리 필요 X, 스프링에 의존
+//        @Autowired
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
+        // 기능이 단순하다.
+        // 별도의 라이브러리가 필요하다.
+        // 자바 표준이르모 스프링이 아닌 다른 컨테이너에서도 사용할 수 있다.
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
+//            PrototypeBean prototypeBean = prototypeBeanProvider.getObject(); // ObjectProvider, ObjectFactory
+            PrototypeBean prototypeBean = prototypeBeanProvider.get(); // JSR-330 Provider javax.inject
             prototypeBean.addCount();
-            int count = prototypeBean.getCount();
-            return count;
+            return prototypeBean.getCount();
         }
     }
 
