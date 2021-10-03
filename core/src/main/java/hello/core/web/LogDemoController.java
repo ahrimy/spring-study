6664bc600c582cbb2f62081ec8e95572bb2bd568
ahrimy <dlsdjdkfla@gmail.com>
2021-10-03 22:19:57 +0900

    웹스코프 - 스코프와 Proxy

--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -15,14 +15,18 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    //    private final MyLogger myLogger; // request scope 는 HTTP 요청이 있을때 하나씩 생성된다 -> 애플리케이션 실행 시점에 생성되어있지 않다.
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    // request scope 는 HTTP 요청이 있을때 하나씩 생성된다 -> 애플리케이션 실행 시점에 생성되어있지 않다.
+    // ObjectProvider 를 사용해서 DL 하면 된다.
+    // 또는 Scope 에 proxyMode 를 추가한다. -> 가짜 myLogger 를 우선 만든다 -> 실제 기능을 요청할때, 진짜를 찾는다.
+    private final MyLogger myLogger;
+//    private final ObjectProvider<MyLogger> myLoggerProvider;
+
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
+//        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
