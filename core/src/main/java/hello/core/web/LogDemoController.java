1c0dbe226f3dc88258f232be9bb286e5de7c292f
ahrimy <dlsdjdkfla@gmail.com>
2021-10-03 22:07:08 +0900

    웹스코프 - 스코프와 Provider

---
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,33 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.inject.Provider;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    //    private final MyLogger myLogger; // request scope 는 HTTP 요청이 있을때 하나씩 생성된다 -> 애플리케이션 실행 시점에 생성되어있지 않다.
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+
+}
